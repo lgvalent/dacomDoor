e4c8c4b23c38e60f28299fd2dc42c712916d14f2
Lucio Valentin <lgvalentin@utfpr.edu.br>
2019-08-26 05:23:07 -0300

    New isDoorOpened sensor in GPIO 16 pin in v5b

--- rasp/doorlock/boardModels.py
+++ rasp/doorlock/boardModels.py
@@ -15,14 +15,17 @@ class BoardModel:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.lockRelayPin,GPIO.OUT)
         GPIO.output(self.lockRelayPin,False)
-        GPIO.setup(self.activityLedPin,GPIO.OUT)
-        GPIO.output(self.activityLedPin,False)
-        GPIO.setup(self.pushButtonProgramPin,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(self.pushButtonCommandPin,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.activityLedPin, GPIO.OUT)
+        GPIO.output(self.activityLedPin, False)
+        GPIO.setup(self.pushButtonProgramPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(self.pushButtonCommandPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         if self.speakerPin>0:
-            GPIO.setup(self.speakerPin,GPIO.OUT)
-            GPIO.output(self.speakerPin,False)
+            GPIO.setup(self.speakerPin, GPIO.OUT)
+            GPIO.output(self.speakerPin, False)
 
+        if self.doorOpenedPin>0:
+                GPIO.setup(self.doorOpenedPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    
         self.rfidReader =  RDM6300.RDM6300(self.activityLedPin, self.serialStr)
 
     def blinkActivityLed(self):
@@ -36,6 +39,10 @@ class BoardModel:
 
     def isCommandButtonPushed(self):
         return not GPIO.input(self.pushButtonCommandPin)
+
+    def isDoorOpened(self):
+        if self.doorOpenedPin>0:
+            return not GPIO.input(self.doorOpenedPin)
 
     def __execCommandButtonCallback(self, callbackFunction):
         #Lucio 20190521: Avoid 'switch bounce'
@@ -93,22 +100,23 @@ class BoardModel:
             speakerPwm.start(10)
             time.sleep(delay)
             speakerPwm.stop()
-
-    def __init__(self, lockRelayPin, lockRelayDelay, activityLedPin, pushButtonProgramPin, pushButtonCommandPin, speakerPin, serialStr):
+        
+    def __init__(self, lockRelayPin, lockRelayDelay, activityLedPin, pushButtonProgramPin, pushButtonCommandPin, speakerPin, doorOpenedPin, serialStr):
         self.lockRelayPin = lockRelayPin
         self.lockRelayDelay = lockRelayDelay
         self.activityLedPin = activityLedPin
         self.pushButtonProgramPin = pushButtonProgramPin
         self.pushButtonCommandPin = pushButtonCommandPin
         self.speakerPin = speakerPin
+        self.doorOpenedPin = doorOpenedPin
         self.serialStr = serialStr
         self.locked = True
 
 class BoardModels(Enum):
-    V1 = BoardModel(23,3,24,25,17,-1, '/dev/serial0')
-    V2 = BoardModel(23,3,24,25,17,-1, '/dev/serial0')
-    V3 = BoardModel(23,.1,25,16,12,-1,'/dev/serial0')
-    V4 = BoardModel(23,.1,24,16,25,12,'/dev/serial0')
-    V5 = BoardModel(24,.1,23,12,25,18,'/dev/serial0')
-    V6 = BoardModel(21,.1,16,18,23,12,'/dev/serial0')
+    V1 = BoardModel(23,3,24,25,17,-1,-1, '/dev/serial0')
+    V2 = BoardModel(23,3,24,25,17,-1,-1, '/dev/serial0')
+    V3 = BoardModel(23,.1,25,16,12,-1,-1,'/dev/serial0')
+    V4 = BoardModel(23,.1,24,16,25,12,-1,'/dev/serial0')
+    V5 = BoardModel(24,.1,23,12,25,18,16,'/dev/serial0')
+    V6 = BoardModel(21,.1,16,18,23,12,16,'/dev/serial0')
 
